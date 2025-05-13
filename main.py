98577b612d550db5512f3ee249497a923f1c8ef3
Gleb15920 <113281265+Gleb15920@users.noreply.github.com>
2025-05-13 20:06:19 +0300

    оптимизация

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sqlite3
+import user
+
 
 def make_db():
     conn = sqlite3.connect('users_table.db')
@@ -7,10 +9,16 @@ def make_db():
             CREATE TABLE IF NOT EXISTS users (
                 tg_id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
-                interests_array TEXT NOT NULL
+                interests TEXT NOT NULL
             )
         ''')
     conn.commit()
     conn.close()
 
 
+make_db()
+user1 = user.User(*input().split(sep=", "), input().split(sep=", "))
+user2 = user.User(*input().split(sep=", "), input().split(sep=", "))
+user3 = user.User(*input().split(sep=", "), input().split(sep=", "))
+print(user1.interests)
+print(user2.find_similar_users())
--- user.py
+++ user.py
@@ -2,22 +2,27 @@ import json
 import sqlite3
 
 
+db_table = 'users_table.db'
+
 class User:
     def __init__(self, tg_id, name, interests):
         self.tg_id = tg_id
         self.name = name
         self.interests = interests
-        self.db()
+        self.db_table = db_table
+        self.update()
         # self.photo = photo
 
     def db(self):
-        conn = sqlite3.connect('users.db')
+        conn = sqlite3.connect(self.db_table)
         cursor = conn.cursor()
 
-        # Сохраняем данные
         interests_json = json.dumps(self.interests)
-        cursor.execute('INSERT OR REPLACE INTO users (id, name, interests_array) VALUES (?, ?, ?)',
+        cursor.execute('INSERT OR REPLACE INTO users (tg_id, name, interests) VALUES (?, ?, ?)',
                        (self.tg_id, self.name, interests_json))
+
+        conn.commit()
+        conn.close()
 
     def change_name(self, name):
         pass
@@ -29,12 +34,12 @@ class User:
         pass
 
     def update(self):
-        pass
+        self.db()
 
     def find_similar_users(self):
-        conn = sqlite3.connect('users.db')
+        conn = sqlite3.connect(self.db_table)
         cursor = conn.cursor()
-        cursor.execute('SELECT tg_id, name, interests FROM users WHERE id != ?', (self.tg_id,))
+        cursor.execute('SELECT tg_id, name, interests FROM users WHERE tg_id != ?', (self.tg_id,))
         users = cursor.fetchall()
         conn.close()
 
@@ -49,9 +54,9 @@ class User:
 
 
 def get_user(tg_id):
-    conn = sqlite3.connect('users.db')
+    conn = sqlite3.connect(db_table)
     cursor = conn.cursor()
-    cursor.execute('SELECT tg_id, name, interests FROM users WHERE id = ?', (tg_id,))
+    cursor.execute('SELECT tg_id, name, interests FROM users WHERE tg_id = ?', (tg_id,))
     ans = cursor.fetchone()
     conn.close()
     if ans:

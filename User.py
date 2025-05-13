7da193ae4c246829820a40d7fe77607c9f415b99
Gleb15920 <113281265+Gleb15920@users.noreply.github.com>
2025-05-13 19:50:40 +0300

    оптимизация

--- User.py
+++ User.py
@@ -1,14 +1,25 @@
+import json
+import sqlite3
+
+
 class User:
-    def __init__(self, tg_id, name, photo, interests):
+    def __init__(self, tg_id, name, interests):
         self.tg_id = tg_id
         self.name = name
         self.interests = interests
-        self.photo = photo
+        self.db()
+        # self.photo = photo
 
-    def data_base(self):
-        pass
+    def db(self):
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
 
-    def change_name(self):
+        # Сохраняем данные
+        interests_json = json.dumps(self.interests)
+        cursor.execute('INSERT OR REPLACE INTO users (id, name, interests_array) VALUES (?, ?, ?)',
+                       (self.tg_id, self.name, interests_json))
+
+    def change_name(self, name):
         pass
 
     def change_interests(self):
@@ -16,3 +27,40 @@ class User:
 
     def change_photo(self):
         pass
+
+    def update(self):
+        pass
+
+    def find_similar_users(self):
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT tg_id, name, interests FROM users WHERE id != ?', (self.tg_id,))
+        users = cursor.fetchall()
+        conn.close()
+
+        similarities = []
+        for other_id, name, interests_json in users:
+            other_interests = json.loads(interests_json)
+            similarity = jaccard_similarity(self.interests, other_interests)
+            similarities.append((name, similarity))
+
+        similarities.sort(key=lambda x: x[1], reverse=True)
+        return similarities
+
+
+def get_user(tg_id):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT tg_id, name, interests FROM users WHERE id = ?', (tg_id,))
+    ans = cursor.fetchone()
+    conn.close()
+    if ans:
+        return User(ans[0], ans[1], ans[2])
+    else:
+        return None
+
+def jaccard_similarity(list1, list2):
+    set1, set2 = set(list1), set(list2)
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union if union != 0 else 0
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import sqlite3
 
 def make_db():
-    pass
+    conn = sqlite3.connect('users_table.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                tg_id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                interests_array TEXT NOT NULL
+            )
+        ''')
+    conn.commit()
+    conn.close()
 
-def add_user(tg_id, name, photo, interests):
-    pass
+
